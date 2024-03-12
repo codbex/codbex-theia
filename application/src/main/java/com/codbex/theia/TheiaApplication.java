22c3e935f4dce224ba143a0d6b7a29e2d31007ea
MinaDoncheva <mina.doncheva@icloud.com>
2024-03-12 14:29:42 +0200

    create profile for integration tests

--- application/src/main/java/com/codbex/theia/TheiaApplication.java
+++ application/src/main/java/com/codbex/theia/TheiaApplication.java
@@ -13,10 +13,6 @@ package com.codbex.theia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
