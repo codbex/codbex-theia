f1fb4bef7549a8819576b3fa250919bbb21533f1
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-31 16:11:25 +0300

    add own terminal it

---
+++ integration-tests/src/test/java/com/codbex/theia/integration/tests/TerminalRestConfig.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2010-2025 Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: Eclipse Dirigible contributors SPDX-License-Identifier: EPL-2.0
+ */
+package com.codbex.theia.integration.tests;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@TestConfiguration
+class TerminalRestConfig {
+
+    @RestController
+    static class TerminalTestRest {
+        public static final String TEST_PATH = "/services/core/version/terminal/api/it";
+        private static boolean called = false;
+
+        static boolean isCalled() {
+            return called;
+        }
+
+        @GetMapping(TEST_PATH)
+        String test() {
+            called = true;
+            return "Hello";
+        }
+    }
+}
---
+++ integration-tests/src/test/java/com/codbex/theia/integration/tests/TheiaTerminalIT.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2025 Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: Eclipse Dirigible contributors SPDX-License-Identifier: EPL-2.0
+ */
+package com.codbex.theia.integration.tests;
+
+import org.eclipse.dirigible.tests.framework.HtmlAttribute;
+import org.eclipse.dirigible.tests.framework.HtmlElementType;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+@Import({TerminalRestConfig.class, TestConfigurations.class})
+public class TheiaTerminalIT extends TheiaIntegrationTest {
+
+    @LocalServerPort
+    private int localServerPort;
+
+    @Test
+    void testTerminalWorks() {
+        ide.openHomePage();
+
+        String testRest = "http://localhost:" + localServerPort + TerminalRestConfig.TerminalTestRest.TEST_PATH;
+        enterCommand("wget -qO- " + testRest);
+
+        await().pollInterval(1, TimeUnit.SECONDS)
+               .atMost(30, TimeUnit.SECONDS)
+               .until(() -> TerminalRestConfig.TerminalTestRest.isCalled());
+    }
+
+    private void enterCommand(String command) {
+        this.browser.clickOnElementByAttributePattern(HtmlElementType.CANVAS, HtmlAttribute.CLASS, "xterm-link-layer");
+        this.browser.type(command);
+        this.browser.pressEnter();
+    }
+
+}
